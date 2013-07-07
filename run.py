b6cf5b2a52816b285a037fec3508074af2dae432
ecko <jmartin@ohaiworld.net>
2013-07-07 12:55:43 -0500

    added options for creating servers and listing flavors/os

--- run.py
+++ run.py
@@ -18,19 +18,19 @@ def ParseArgs():
                         help="Account's API key",
                         required=True,
                        )
-    """
     parser.add_argument('--name', '-n',
                         help="Server or Cluser name",
                        )
     parser.add_argument('--web-nodes', '-wn',
                         help="Number of webnodes, OS and Size",
                         nargs=3,
+                        metavar=('nodes', 'OS', 'size'),
                        )
     parser.add_argument('--mysql-nodes', '-mn',
                         help="Number of webnodes, OS and Size",
                         nargs=3,
+                        metavar=('nodes', 'OS', 'size'),
                        )
-    """
     parser.add_argument('--next-gen', '-ng',
                         help="List Next Gen servers",
                         action="store_true",
@@ -49,6 +49,11 @@ def ParseArgs():
                        )
     parser.add_argument('--container', '-c',
                         help="List the files in the Cloud Files container",
+                       )
+    parser.add_argument('--list', '-l',
+                        nargs=1,
+                        choices=['os', 'sizes'],
+                        help="List the available OS or Server Sizes",
                        )
     return parser.parse_args()
 
@@ -69,10 +74,20 @@ if args.files:
         container = '/' + args.container + '/'
         api.parseCloudFilesInContainer(api.getDetailsByEndpoint("cloudFiles", ['token'], 'GetCloudFilesContainers', args.region, container))
 if args.next_gen:
-        print "Cloud Servers in " + args.region.upper()
-        print '-' * 40
-        api.parseCurrentServers(api.getDetailsByEndpoint("cloudServersOpenStack", ['token'], 'getInstalledServers', args.region))
+    print "Cloud Servers in " + args.region.upper()
+    print '-' * 40
+    api.parseCurrentServers(api.getDetailsByEndpoint("cloudServersOpenStack", ['token'], 'getInstalledServers', args.region))
 if args.first_gen:
-        api.prettyPrint(api.getFirstGenServers(api.tenant_id, ['token']))
-
-print '\n'
+    api.prettyPrint(api.getFirstGenServers(api.tenant_id, ['token']))
+if args.list:
+    if 'os' in args.list:
+        api.prettyPrint(api.getDetailsByEndpoint("cloudServersOpenStack", ['token'], 'getOS', args.region))
+if args.web_nodes:
+    if args.name:
+        node_count = 0
+        nodes = args.web_nodes[0]
+        os = args.web_nodes[1]
+        size = args.web_nodes[2]
+        for node in range(int(nodes)):
+            server_name = 'WEB ' + "{0:02d} ".format(node + 1) + args.name
+            print api.createServer(server_name, os, size)
